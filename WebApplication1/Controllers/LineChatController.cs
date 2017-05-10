3a5e3b9e058a5eeb2aa2af5feb3b75a6b1a1c79f
James992927108 <sam992927108@gmail.com>
2017-05-11 00:42:06 +0800

    practice

--- WebApplication1/Controllers/LineChatController.cs
+++ WebApplication1/Controllers/LineChatController.cs
@@ -28,7 +28,7 @@ namespace WebApplication1.Controllers
 
                 string groupId,roomId, userId, souretype;
 
-                string messagetype, id, text, title, address, latitude, longitude;
+                string messagetype, messageid, text, title, address, latitude, longitude;
                 int packageId, stickerId;
 
                 string data;
@@ -41,11 +41,11 @@ namespace WebApplication1.Controllers
 
                 //groupId = ReceivedMessage.events[0].source.groupId.ToString();
                 //roomId = ReceivedMessage.events[0].source.roomId.ToString();
-                //userId = ReceivedMessage.events[0].source.userId;
+                userId = ReceivedMessage.events[0].source.userId;
                 //souretype = ReceivedMessage.events[0].source.type.ToString();
 
                 //messagetype = ReceivedMessage.events[0].message.type.ToString();
-                id = ReceivedMessage.events[0].message.id.ToString();
+                messageid = ReceivedMessage.events[0].message.id.ToString();
                 text = ReceivedMessage.events[0].message.text;
                 //title = ReceivedMessage.events[0].message.title.ToString();
                 //address = ReceivedMessage.events[0].message.address.ToString();
@@ -53,12 +53,18 @@ namespace WebApplication1.Controllers
                 //longitude = ReceivedMessage.events[0].message.longitude.ToString();
                 //packageId = ReceivedMessage.events[0].message.packageId;
                 //stickerId = ReceivedMessage.events[0].message.stickerId;
-
                 //data = ReceivedMessage.events[0].postback.data.ToString();
 
-                Message = "你說了:" + text +"----id:"+id;
+                Message = "你說了 : " + text + "\r\nmessageid : " + messageid + "\r\nuserId : " + userId + "\r\nTime : " + DateTime.Now.ToString();
+
+                if (text == "test")
+                {
+                    isRock.LineBot.Utility.ReplyMessage(ReplyToken, "test success\r\n" + Message, ChannelAccessToken);
+                }
                 //回覆用戶
                 isRock.LineBot.Utility.ReplyMessage(ReplyToken, Message, ChannelAccessToken);
+                
+
                 //回覆API OK
                 return Ok();
             }
