5cb547122f65c136bc7480aafd49732f56b4783e
James992927108 <sam992927108@gmail.com>
2017-05-11 00:14:32 +0800

    LineBot only can reply the same text what i input

---
+++ README.md
@@ -0,0 +1,12 @@
+
+<br/>相關使用說明請參考LineBot系列文章，Index位於:
+<br/>http://studyhost.blogspot.tw/2017/01/blog-post.html
+
+<br/>使用此範例請務必參考上述連結中的Blog相關文章
+<br/>自行申請LineBot Account並替換程式碼中的Token與UserId，才能正確執行
+
+原本
+https://lateatnight.herokuapp.com:443/callback
+
+新的
+http://fortestlinebot.azurewebsites.net
---
+++ WebApplication1/Controllers/LineChatController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class LineChatController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult POST()
+        {
+            string ChannelAccessToken = "XYaRE0qBH4/2oKlLnvo37e20XZDV6Oz104ynfWt/jwIKC1kQC+MN/Ryvj8jVo0W/Gnej675fXB6jrFQkMt5xz/nx1aGFsLlFi8igSkNttLYJdG4U1+LSl9dcXhPizM3XjD3dNFNjDKJCzYSaHmdbJQdB04t89/1O/w1cDnyilFU=";
+
+
+            //isRock.LineBot.Utility.PushMessage(UserId, 要傳送的訊息, AccessToken);
+            try
+            {
+                //取得 http Post RawData(should be JSON)
+                string postData = Request.Content.ReadAsStringAsync().Result;
+                //剖析JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                //回覆訊息
+
+                string  ReplyToken, timestamp, type;
+
+                string groupId,roomId, userId, souretype;
+
+                string messagetype, id, text, title, address, latitude, longitude;
+                int packageId, stickerId;
+
+                string data;
+
+                string Message;
+
+                //timestamp = ReceivedMessage.events[0].timestamp.ToString();
+                ReplyToken = ReceivedMessage.events[0].replyToken;
+                //type = ReceivedMessage.events[0].type.ToString();
+
+                //groupId = ReceivedMessage.events[0].source.groupId.ToString();
+                //roomId = ReceivedMessage.events[0].source.roomId.ToString();
+                //userId = ReceivedMessage.events[0].source.userId;
+                //souretype = ReceivedMessage.events[0].source.type.ToString();
+
+                //messagetype = ReceivedMessage.events[0].message.type.ToString();
+                id = ReceivedMessage.events[0].message.id.ToString();
+                text = ReceivedMessage.events[0].message.text;
+                //title = ReceivedMessage.events[0].message.title.ToString();
+                //address = ReceivedMessage.events[0].message.address.ToString();
+                //latitude = ReceivedMessage.events[0].message.latitude.ToString();
+                //longitude = ReceivedMessage.events[0].message.longitude.ToString();
+                //packageId = ReceivedMessage.events[0].message.packageId;
+                //stickerId = ReceivedMessage.events[0].message.stickerId;
+
+                //data = ReceivedMessage.events[0].postback.data.ToString();
+
+                Message = "你說了:" + text +"----id:"+id;
+                //回覆用戶
+                isRock.LineBot.Utility.ReplyMessage(ReplyToken, Message, ChannelAccessToken);
+                //回覆API OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Ok();
+            }
+        }
+    }
+}
